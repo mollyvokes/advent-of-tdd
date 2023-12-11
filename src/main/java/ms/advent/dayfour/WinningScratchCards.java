5441bb74a1e1801ca9a632d48e252e2c334fbdff
mollyvokes <mollyrvokes@gmail.com>
2023-12-11 17:37:50 +0000

    day 4 p2

--- src/main/java/ms/advent/dayfour/WinningScratchCards.java
+++ src/main/java/ms/advent/dayfour/WinningScratchCards.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 
 public class WinningScratchCards {
@@ -35,17 +35,72 @@ public class WinningScratchCards {
         return cardScore;
     }
 
+    public static int numCardWinners(String card) {
+        String[] winningNumbers = getWinningNumbers(card);
+        String[] cardNumbers = getCardNumbers(card);
+
+        int cardWinners = 0;
+
+        for (String number: cardNumbers) {
+            if (List.of(winningNumbers).contains(number)) {
+                cardWinners++;
+            }
+        }
+        return cardWinners;
+    }
+
+    public static List<Integer> winningCardCopies(int index, String card, List<Integer> copiesList) {
+
+        int cardNumber = index+1;
+
+        List<Integer> collectedCards = new ArrayList<>();
+        collectedCards.add(cardNumber);
+        int winningNumbers = numCardWinners(card);
+        int repeats = findRepeats(cardNumber, copiesList);
+
+        // do this as many times as the card number currently appears in the copies list - use chatgpt to find count of occurance in list
+        for (int r = 0; r < repeats+1; r++) {
+            for (int i = cardNumber + 1; i < cardNumber + winningNumbers + 1; i++) {
+                collectedCards.add(i);
+            }
+        }
+        return collectedCards;
+    }
+
+    public static int findRepeats(int cardNum, List<Integer> copiesList) {
+        int repeats = 0;
+        for (int c: copiesList) {
+            if (c == cardNum) {
+                repeats++;
+            }
+        }
+        return repeats;
+    }
+
+    public static List<Integer> findAllCardsAndCopies(List<String> allCards) {
+        List<Integer> allCopies = new ArrayList<>();
+        for (int i=0; i < allCards.size(); i++) {
+            allCopies.addAll(winningCardCopies(i, allCards.get(i), allCopies));
+        }
+        return allCopies;
+    }
+
 
     public static void main(String[] args) throws IOException {
+
         Path path = Paths.get("src/main/resources/day-four-scratch-cards.txt");
         List<String> allCards = Files.readAllLines(path);
 
-        int winningTotal = 0;
-        for (String card: allCards) {
-            winningTotal += cardPoints(card);
-        }
+        List<Integer> totalCardCollection = findAllCardsAndCopies(allCards);
 
-        System.out.println("The total of winning cards is:" + winningTotal);
+        System.out.println("the number of cards won is: " + totalCardCollection.size());
+
+//        int winningTotal = 0;
+//        for (String card: allCards) {
+//            winningTotal += numCardWinners(card);
+//        }
+//
+//        System.out.println("The total of winning cards is:" + winningTotal);
 
     }
 }
--- src/test/java/ms/advent/dayfour/TestWinningScratchCardsShould.java
+++ src/test/java/ms/advent/dayfour/TestWinningScratchCardsShould.java
@@ -1,9 +1,14 @@
 package ms.advent.dayfour;
 
+import ms.advent.dayone.CalibrationCode;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Stream;
 
 import static org.hamcrest.JMock1Matchers.equalTo;
@@ -12,7 +17,7 @@ import static org.hamcrest.Matchers.comparesEqualTo;
 
 public class TestWinningScratchCardsShould {
 
-    static Stream<Arguments> getTestCases() {
+    static Stream<Arguments> getPointsTestCases() {
         return Stream.of(
                 Arguments.arguments("Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53", 8),
                 Arguments.arguments("Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19", 2),
@@ -22,10 +27,41 @@ public class TestWinningScratchCardsShould {
     }
 
     @ParameterizedTest
-    @MethodSource("getTestCases")
+    @MethodSource("getPointsTestCases")
     void validate_test_cards_against_expected(String card, int expected) {
         assertThat(WinningScratchCards.cardPoints(card), comparesEqualTo(expected));
     }
+
+    static Stream<Arguments> getNumTestCases() {
+        return Stream.of(
+                Arguments.arguments("Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53", 4),
+                Arguments.arguments("Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19", 2),
+                Arguments.arguments("Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1", 2),
+                Arguments.arguments("Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36", 0)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("getNumTestCases")
+    void validate_test_number_cards_against_expected(String card, int expected) {
+        assertThat(WinningScratchCards.numCardWinners(card), comparesEqualTo(expected));
+    }
+
+
+    @Test
+    void get_correct_num_repeats() {
+        List<Integer> testStartingCards = new ArrayList<>();
+        testStartingCards.add(1);
+        testStartingCards.add(2);
+        testStartingCards.add(3);
+        testStartingCards.add(2);
+        testStartingCards.add(3);
+        testStartingCards.add(2);
+        assertThat(WinningScratchCards.findRepeats(2, testStartingCards), Matchers.equalTo(3));
+    }
+
+
+
 }
 
 
